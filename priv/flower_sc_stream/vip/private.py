a0b07861e50ea2e26802894fdb48092fae69cfc2
anatolii2makaroff <anatolii2makaroff@gmail.com>
2018-12-07 21:32:59 +0000

    add vip strategy

---
+++ priv/flower_sc_stream/vip/private.py
@@ -0,0 +1,10 @@
+
+_VIP_CMDS = [
+    "sudo ifconfig {iface}:0 down",
+    "sudo ifconfig {iface}:0 {vip} netmask 255.255.255.255 broadcast {vip}",
+    "sudo arping -I {iface} -c 2 -s {vip} {vip}"
+]
+
+_RSERV_CMDS = [
+    "sudo ifconfig {iface}:0 down",
+]
