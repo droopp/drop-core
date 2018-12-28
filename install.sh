34c437e7ebb71ef5013b7911f32115a2bea91c3f
anatolii2makaroff <anatolii2makaroff@gmail.com>
2018-12-28 11:13:07 +0000

    ch repo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Install DROP Core
 #
 
-yum install -y http://localhost:8888/RPMS/erlang-20.0.0-1.x86_64.rpm
+yum install -y https://localhost/RPMS/erlang-20.0.0-1.x86_64.rpm
 export PATH=/opt/erlang/bin/:$PATH
 
 cd /opt/drop-core
